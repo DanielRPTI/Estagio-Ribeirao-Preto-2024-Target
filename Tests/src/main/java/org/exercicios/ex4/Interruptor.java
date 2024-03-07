7b0948248d3b3450341eeb193b9a9ab6473527ee
DanielRPTI <drjunior988@hotmail.com>
2024-03-06 22:36:51 -0300

    Exercicio 4 Interruptores

---
+++ Tests/src/main/java/org/exercicios/ex4/Interruptor.java
@@ -0,0 +1,24 @@
+package org.exercicios.ex4;
+
+public class Interruptor {
+    /*
+4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente.
+Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+
+Primeiro iria deixar desligado o primeiro interruptor ,
+ligaria o terceiro interruptor  e o segundo interruptor juntos,
+desligaria o interruptor dois e iria fazer a primeira ida a sala,
+se a lampada estivesse apagada e fria seria respectiva ao primeiro interruptor
+ou se a lampa estivesse apagada mas quente seria o nosso segundo interruptor devido o tempo que deixamos ligada e desligamos depois
+se a lampada estivesse acesa  seria o terceiro interruptor
+
+Na Segunda Ida se eu tivesse descoberto um dos interruptores
+Caso seja correspondente o terceiro interruptor
+eu verifico se nas salas apagadas se elas atendem as condições
+se a lampada estivesse apagada e fria primeiro interruptor
+se quente segundo interruptor
+
+*/
+}
