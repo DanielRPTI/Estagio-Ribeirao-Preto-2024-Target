e566260a69ef280733be76a2574aaa92a156da24
DanielRPTI <drjunior988@hotmail.com>
2024-03-06 23:11:44 -0300

    Encerrando projeto após conclusão de todas as tarefas

--- Tests/src/main/java/org/exercicios/ex5/StringInvertida.java
+++ Tests/src/main/java/org/exercicios/ex5/StringInvertida.java
@@ -14,10 +14,15 @@ public class StringInvertida {
         System.out.println("{" + revertString(texto) + "}");
     }
 
+    /*Class responsavel por realizar a reversão
+      realizamos o tratamento de se caso for informado um valor null ele irá retornar a variavel vazia
+      se não ele entra no for e começa a iterar nossa string a partir de seu ultimo indice , a cada interação sendo atribuida
+      diretamente em nossa variavel de saída (reverseString) até que nosso laço rompa chegando no indice 0
+    */
     public static String revertString(String minhaString) {
+        String reverseString = "";
         if (minhaString == null)
             return minhaString;
-        String reverseString = "";
         for (int i = minhaString.length() - 1; i >= 0; i--) reverseString = reverseString + minhaString.charAt(i);
         return reverseString;
     }
