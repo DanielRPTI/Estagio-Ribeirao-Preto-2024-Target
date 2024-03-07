2775bc50023ddb717288e285ba6d69b6ed0e1ff5
DanielRPTI <drjunior988@hotmail.com>
2024-03-06 21:42:50 -0300

    Finalizando Exercicio 02-Calculo Fibonacci

--- Tests/src/main/java/org/exercicios/ex2/Fibonacci.java
+++ Tests/src/main/java/org/exercicios/ex2/Fibonacci.java
@@ -8,11 +8,24 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int entrada ;
+        int primeiroValor ;
+        int segundoValor;
+        int valorAuxiliar = 0;
+        int soma = 0;
 
         System.out.println("--\tCalculo Fibonacci\t--");
         System.out.println("Por favor informe um número para realizar o cálculo: ");
-        entrada = sc.nextInt();
-        
+        primeiroValor = sc.nextInt();
+        System.out.println("Por favor informe outro número para realizar o cálculo: ");
+        segundoValor = sc.nextInt();
+
+        //laço para realizarmos nosso calculo e ir atribuindo as variaveis sequencialmente, até estourar o laço
+        for (int i = 0; i < 10; i++){
+            System.out.print("{" + valorAuxiliar + "}");
+            valorAuxiliar = primeiroValor + segundoValor;
+            primeiroValor = segundoValor;
+            segundoValor = valorAuxiliar;
+
+        }
     }
 }
